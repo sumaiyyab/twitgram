99e28d08fd9e13702e715c59033dc1e4b5fb5956
Sumaiyya Burney <33647766+sumaiyyab@users.noreply.github.com>
2020-05-18 16:06:23 -0700

    Updates to module to be used by other file

--- tweets2jsonl.py
+++ tweets2jsonl.py
@@ -2,28 +2,25 @@ import searchtweets as st
 import json
 import sys
 
-if len(sys.argv) > 1:
-	user = sys.argv[1]
-else:
-	user = input("User to retrieve: ")
+def loadTweets (user):
+	print('Loading tweets from ' + user + '...')
+	search_args = st.load_credentials("twitter_keys.yaml",
+									yaml_key="search_tweets_api",
+									env_overwrite=False)
 
-search_args = st.load_credentials("twitter_keys.yaml",
-								yaml_key="search_tweets_api",
-								env_overwrite=False)
+	rule = st.gen_rule_payload("from:"+user, 
+							results_per_call=100,
+							from_date="2020-04-18",
+							to_date="2020-05-18"
+							)
 
-rule = st.gen_rule_payload("from:"+user, 
-						results_per_call=100,
-						from_date="2020-04-18",
-						to_date="2020-05-18"
-						)
+	rs = st.ResultStream(rule_payload=rule,
+						max_results=100,
+						**search_args)
 
-rs = st.ResultStream(rule_payload=rule,
-					max_results=100,
-					**search_args)
-
-results = list(rs.stream())
-with open(user+'Twts.jsonl', 'w', encoding='utf-8') as f:
-	for tweet in results:
-		json.dump(tweet, f)
-		f.write('\n')
-print('done - ' + str(len(results)) + " tweets saved")
+	results = list(rs.stream())
+	with open(user+'Twts.jsonl', 'w', encoding='utf-8') as f:
+		for tweet in results:
+			json.dump(tweet, f)
+			f.write('\n')
+	print('done - ' + str(len(results)) + " tweets saved")
